2a156af9c9f93c0958a92ba70484ee960783b4b5
dogyoonlee <nemotio@yonsei.ac.kr>
2021-03-03 15:36:53 +0900

    arguments added

--- visualization_tool/vis_shapenet.py
+++ visualization_tool/vis_shapenet.py
@@ -1,17 +1,40 @@
 import open3d as o3d
 import numpy as np
 import os
+import argparse
+'''
+    {'Airplane': 0,   *** 
+    'Bag': 1, 
+    'Cap': 2, 
+    'Car': 3, 
+    'Chair': 4,        ****
+    'Earphone': 5, 
+    'Guitar': 6, 
+    'Knife': 7, 
+    'Lamp': 8,     ***
+    'Laptop': 9,
+    'Motorbike': 10, 
+    'Mug': 11, 
+    'Pistol': 12, 
+    'Rocket': 13, 
+    'Skateboard': 14, 
+    'Table': 15}
+'''
 
 
-def shapenet():
+def shapenet(object='Airplane', layer='e', layer_num=1, idx=0):
     '''
         Visualization shapenet
         @ https://github.com/dogyoonlee/point_cloud_feature_visualization
     '''
-
+    if layer is 'e':
+        filename = '../../feature_vis/2021-3-3-12/Airplane/feature_Object_' + str(
+            idx) + '_' + object + '_Encode_Layer_' + str(layer_num) + '.npy'
+    else:
+        filename = '../../feature_vis/2021-3-3-12/Airplane/feature_Object_' + str(
+            idx) + '_' + object + '_Decode_Layer_' + str(layer_num) + '.npy'
     # input: B x N x (3 + 3) ; rgb value format : 0~1
-    data = np.load(
-        '../../feature_vis/2021-3-2-21-25/feature_Decode_Layer_4.npy')
+    data = np.load(filename)
     data = data[0][:][:]
 
     xyzrgb = data[:, :6]
@@ -23,4 +46,13 @@ def shapenet():
 
 
 if __name__ == '__main__':
-    shapenet()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--obj', default='Airplane', type=str)
+    parser.add_argument('--layer', default='e', type=str)
+    parser.add_argument('--layer_num', default=1, type=int)
+    parser.add_argument('--idx', default=0, type=int)
+    args = parser.parse_args()
+    shapenet(object=args.obj,
+             layer=args.layer,
+             layer_num=args.layer_num,
+             idx=args.idx)
